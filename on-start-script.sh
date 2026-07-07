8406c437b6ac29a7c5812ade9fb170bf162b9cb1
Myungkyu Han <35263213+MKHan91@users.noreply.github.com>
2026-07-08 00:34:06 +0900

    Refactor on-start-script.sh for new workspace structure
    Updated the script to change working directory and adjust paths for data downloads and unzipping. Removed deprecated commands and added new wget commands for dataset retrieval.

--- on-start-script.sh
+++ on-start-script.sh
@@ -1,31 +1,15 @@
-cd /hom/dev
-git clone https://github.com/MKHan91/VQ-Font.git
-git clone https://github.com/MKHan91/multi-label-dicasting
+cd /workspace
+
 git clone https://github.com/Semantic-Segmentation-Research/ssl-semantic-segmentation
-
-git remote set-url origin https://<GIT USERNAME>:<GIT TOKEN>@github.com/MKHan91/VQ-Font.git
 git config --global credential.helper store
+git config --global user.name "MKHan91"
+git config --global user.email "audrbz@naver.com"
 
-mkdir /home/dev/ssl-semantic-segmentation/pretrained
-gdown --id 1Rx0legsMolCWENpfvE2jUScT3ogalMO8 -O /home/dev/ssl-semantic-segmentation/pretrained/resnet101.pth
+mkdir -p /workspace/data
+wget https://huggingface.co/datasets/mkhan91/Cityscapes/resolve/main/leftImg8bit.zip -O /home/dev/data/leftImg8bit.zip
+wget https://huggingface.co/datasets/mkhan91/Cityscapes/resolve/main/gtFine_trainvaltest.zip -O /home/dev/data/gtFine_trainvaltest.zip
 
-mkdir -p /home/dev/data
-
-gdown --id 1mFqk39EwQLQsvo9KSW36QxSohjPongbg -O /home/dev/data/gtFine_trainvaltest.zip
-gdown --id 1ux6yQeNkRw1KMsuhSeELD7wIkKZqgoLW -O /home/dev/data/leftImg8bit.zip
-
-cd /home/dev/data
+cd /workspace/data
 unzip gtFine_trainvaltest.zip
 unzip leftImg8bit.zip
-
-mkdir -p /home/dev/Project
-mv /home/dev/VQ-Font /home/dev/Project/VQ-Font
-
-gdown --id 1AXPuyS7SJ5r9F8rWcAB-GikTYAiN6cKT -O /hom/dev/Project/VQ-Font/taming/20260117_vq_gan_exp.zip
-cd /hom/dev/Project/VQ-Font/taming
-unzip 20260117_vq_gan_exp.zip
-
-gdown --id 1WFwuW9I5ZbquxH53dipDmN2gI1XOygVA -O /home/dev/Project/VQ-Font/datasets/20260119_dataset.zip
-cd /home/dev/Project/VQ-Font/datasets
-unzip 20260119_dataset.zip
 
